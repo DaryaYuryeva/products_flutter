0a009be95d64650ec7d32658cbe5cba4dcbd7b87
Darya Yuryeva <79998801+DaryaYuryeva@users.noreply.github.com>
2024-12-21 18:09:48 +0300

    Create mock_data.dart

---
+++ lib/data_source/mock_data.dart
@@ -0,0 +1,48 @@
+import '../model/product.dart';
+
+final List<Product> mockData = [
+  const Product(
+    id: 1,
+    title: 'title 1',
+    description: 'description 1',
+    price: 99.99,
+    images: [
+      'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png'
+    ],
+    thumbnail:
+    'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png',
+  ),
+  const Product(
+    id: 2,
+    title: 'title 2',
+    description: 'description 2',
+    price: 9.99,
+    images: [
+      'https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png'
+    ],
+    thumbnail:
+    'https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png',
+  ),
+  const Product(
+    id: 3,
+    title: 'title 3',
+    description: 'description 3',
+    price: 4.99,
+    images: [
+      'https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/1.png'
+    ],
+    thumbnail:
+    'https://cdn.dummyjson.com/products/images/beauty/Eyeshadow%20Palette%20with%20Mirror/thumbnail.png',
+  ),
+  const Product(
+    id: 4,
+    title: 'title 4',
+    description: 'description 4',
+    price: 13.50,
+    images: [
+      'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png'
+    ],
+    thumbnail:
+    'https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png',
+  ),
+];
