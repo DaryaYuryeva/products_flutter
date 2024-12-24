6a0877cc3876eb8ef0729343baa7e60729d10796
Darya Yuryeva <79998801+DaryaYuryeva@users.noreply.github.com>
2024-12-24 23:32:02 +0300

    Update navigation_screen.dart

--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../bloc/cart/cart_cubit.dart';
 import 'cart_screen.dart';
 import 'products_screen.dart';
 
@@ -15,6 +17,8 @@ class _NavigationScreenState extends State<NavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final int cartItemsCount = context.watch<CartCubit>().state.items.length;
+
     return Scaffold(
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
@@ -23,17 +27,29 @@ class _NavigationScreenState extends State<NavigationScreen> {
           });
         },
         selectedIndex: _currentPageIndex,
-        destinations: const <Widget>[
-          NavigationDestination(
+        destinations: <Widget>[
+          const NavigationDestination(
             selectedIcon: Icon(Icons.home),
             icon: Icon(Icons.home_outlined),
             label: 'Home',
           ),
-          NavigationDestination(
-            selectedIcon: Icon(Icons.shopping_cart),
-            icon: Icon(Icons.shopping_cart_outlined),
-            label: 'Cart',
-          ),
+          cartItemsCount == 0
+              ? const NavigationDestination(
+                  selectedIcon: Icon(Icons.shopping_cart),
+                  icon: Icon(Icons.shopping_cart_outlined),
+                  label: 'Cart',
+                )
+              : NavigationDestination(
+                  selectedIcon: Badge(
+                    label: Text('$cartItemsCount'),
+                    child: const Icon(Icons.shopping_cart),
+                  ),
+                  icon: Badge(
+                    label: Text('$cartItemsCount'),
+                    child: const Icon(Icons.shopping_cart_outlined),
+                  ),
+                  label: 'Cart',
+                ),
         ],
       ),
       body: <Widget>[
